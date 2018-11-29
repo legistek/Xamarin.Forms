9fed67fee9352d8a854a8b11052b0a519ed1daa6
Peter N. Moore <pmoore@legistek.com>
2018-11-29 12:08:10 -0600

    Implement MultiBinding for OneWay and TwoWay modes

---
+++ Xamarin.Forms.Core/IMultiValueConverter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Xamarin.Forms
+{
+	public interface IMultiValueConverter
+	{
+		object Convert(object[] values, Type targetType, object parameter, CultureInfo culture);
+		object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture);
+	}
+}
